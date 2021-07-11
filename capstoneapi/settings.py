4b038e9476441daf5be2e76cdd112f33f1ef03c1
Matthew Turner <turnermb92@gmail.com>
2021-07-11 13:13:41 -0500

    initial commit, adds get and post to / route

--- capstoneapi/settings.py
+++ capstoneapi/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e=!irmx!2b*(y&^8ym3vwg66l-ywhd-p6$vf^)g(d!t!(krj7a'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Dotenv
+
+from dotenv import load_dotenv
+load_dotenv()
