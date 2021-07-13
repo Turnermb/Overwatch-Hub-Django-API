ed19e2ab881f7315af323c205419033669f2e08f
Matthew Turner <turnermb92@gmail.com>
2021-07-12 20:13:49 -0500

    changes database to postgresql

--- capstoneapi/settings.py
+++ capstoneapi/settings.py
@@ -82,8 +82,19 @@ WSGI_APPLICATION = 'capstoneapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'test',
+
+        'USER': 'test',
+
+        'PASSWORD': 'test',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
